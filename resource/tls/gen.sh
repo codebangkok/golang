13810e0178ab80c4ac79fad5506290545b990533
Surasuk Oakkharaamonphong <surasuk612@gmail.com>
2022-04-05 12:50:18 +0700

    update tls gen.sh

--- resource/tls/gen.sh
+++ resource/tls/gen.sh
@@ -5,9 +5,9 @@
 # ca.key: Certificate Authority private key file (this shouldn't be shared in real-life)
 # ca.crt: Certificate Authority trust certificate (this should be shared with users in real-life)
 # server.key: Server private key, password protected (this shouldn't be shared)
+# server.pem: Conversion of server.key into a format gRPC likes (this shouldn't be shared)
 # server.csr: Server certificate signing request (this should be shared with the CA owner)
 # server.crt: Server certificate signed by the CA (this would be sent back by the CA owner) - keep on server
-# server.pem: Conversion of server.key into a format gRPC likes (this shouldn't be shared)
 
 # Summary 
 # Private files: ca.key, server.key, server.pem, server.crt
@@ -23,11 +23,11 @@ openssl req -passin pass:1111 -new -x509 -sha256 -days 365 -key ca.key -out ca.c
 # Step 2: Generate the Server Private Key (server.key)
 openssl genrsa -passout pass:1111 -des3 -out server.key 4096
 
-# Step 3: Get a certificate signing request from the CA (server.csr)
+# Step 3: Convert the server certificate to .pem format (server.pem) - usable by gRPC
+openssl pkcs8 -topk8 -nocrypt -passin pass:1111 -in server.key -out server.pem
+
+# Step 4: Get a certificate signing request from the CA (server.csr)
 openssl req -passin pass:1111 -new -sha256 -key server.key -out server.csr -subj "/CN=${SERVER_CN}" -config ssl.cnf
 
-# Step 4: Sign the certificate with the CA we created (it's called self signing) - server.crt
+# Step 5: Sign the certificate with the CA we created (it's called self signing) - server.crt
 openssl x509 -req -passin pass:1111 -sha256 -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt -extensions req_ext -extfile ssl.cnf
-
-# Step 5: Convert the server certificate to .pem format (server.pem) - usable by gRPC
-openssl pkcs8 -topk8 -nocrypt -passin pass:1111 -in server.key -out server.pem
